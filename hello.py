dd1aee20f072e92de6b03e64755034ae154543ea
ShaDC24 <shatabdi198dc@gmail.com>
2024-09-29 10:45:38 +0600

    message

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
 print("Hello hi Bye Bye!!!")
+
+#git init
+#git add
+#git commit -m "message"
+#git status
+#git log
+
+
+
+#wsl
+#ls
+#ls -all
+
+
+#cd .git
+#ls
+#logout
+
+
+#git add-A ..........project er sobgulo file will be checked
+#git add . -----------current directory er file gulo track rakhbe
+#git add -u -------------age chilo emon file jegulo ke modify kora hoeche
+#kebol tader kei  add korbe.
