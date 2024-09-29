7fb02b830ee29f45f0b34cf46c3d9f3434c04715
ShaDC24 <shatabdi198dc@gmail.com>
2024-09-29 11:55:23 +0600

    Added I lova in hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 print("Hello hi Bye Bye!!!")
+print("I lova CSE,BUET.")
 
 #git init
 #git add
@@ -58,3 +59,18 @@ print("Hello hi Bye Bye!!!")
 
 
 
+
+#Branching
+#in maximum case there are multiple collaborators who work simultaneously.
+#in git branch ...amra asole more than 1 branch crete korbo ...nijeder moto kaj korte thakbo..
+#at last mereg korbo
+
+#git branch 
+#ei command run korle amra amader sobgulo branch dekhte pabo.
+#git branch Feature-1
+#amra feature-1 name e  akta branch crete korlam
+#je branch er pashe * sei branch e amara at present ache seta bujhay.
+#amra git push korleo ..amder repository te new branch ta addd hobe na.
+
+#git checkout Feature-1
+#ei command die amra branch e move korte pari
